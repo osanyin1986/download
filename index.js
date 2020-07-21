3f0961b381caa0ab556006b4782356cf75c2af99
osanyin1986 <61332416+osanyin1986@users.noreply.github.com>
2020-07-22 00:46:23 +0100

    initial
    yes

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var convertBtn = function () {
+
+document.querySelector('.convert-button');
+}
+var URLinput = function () {
+  document.querySelector('.URL-input');
+
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+}
+function sendURL(URL) {
+    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+}
